f877718af7aa4940268225a2d1f536171f1ce8ef
MMIX2009 <btshilombo@comcast.net>
2023-02-11 22:05:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
 import streamlit as st
 import time
 
+if "photo" not in st.session_state:
+  st.session_state["photo"] = "not done"
+  
 col1, col2, col3 = st.columns([1,2,1])
 
 col1.markdown("## Welcome to my app!")
 col1.markdown(" Here is some info about the app")
 
-uploaded_photo = col2.file_uploader("Upload an image")
-camera_photo = col2.camera_input("Take a picture")
+def change_photo_state():
+  st.session_state["photo"] = "done"
 
-progress_bar = col2.progress(0);
+uploaded_photo = col2.file_uploader("Upload an image", on_change=change_photo_state)
+camera_photo = col2.camera_input("Take a picture", on_change=change_photo_state)
 
-for perc_completed in range(100):
-  time.sleep(0.05)
-  progress_bar.progress(perc_completed +1)
-  
-if uploaded_photo:
-  col2.success("Photo uploaded sucessfully!")
-  
-col3.metric(label="Temperature", value="60 °C", delta="-3 °C")
+if st.session_state["photo"] == "done"
+  progress_bar = col2.progress(0);
 
-with st.expander('Click to learn more'):
-  st.write('Hello, here are more details on this topic that you are interested in.')
-  
-  if uploaded_photo is None:
-    st.image(camera_photo)
-  else:
-    st.image(uploaded_photo)
+  for perc_completed in range(100):
+    time.sleep(0.05)
+    progress_bar.progress(perc_completed +1)
+
+  if uploaded_photo:
+    col2.success("Photo uploaded sucessfully!")
+
+  col3.metric(label="Temperature", value="60 °C", delta="-3 °C")
+
+  with st.expander('Click to learn more'):
+    st.write('Hello, here are more details on this topic that you are interested in.')
+
+    if uploaded_photo is None:
+      st.image(camera_photo)
+    else:
+      st.image(uploaded_photo)
+ else:
+  pass
