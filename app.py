d31e49a4fb33c081a1dd542ab00e6f3cea25b220
MMIX2009 <btshilombo@comcast.net>
2023-02-11 21:25:01 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+col1, col2, col3 = st.columns([1,2,1])
+
+col1.markdown("# Welcome to my app!")
+col1.markdown(" Here is some info about the app")
+
+uploaded_photo = col2.file_uploader("Upload an image")
+camera_photo = col2.camera_input("Take a picture")
