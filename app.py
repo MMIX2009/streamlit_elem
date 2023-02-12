0c5c18dfacd6e0038b33dbc34c79d681d9892d3b
MMIX2009 <btshilombo@comcast.net>
2023-02-11 21:36:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ for perc_completed in range(100):
   time.sleep(0.05)
   progress_bar.progress(perc_completed +1)
   
-if uploaded_photo != null:
+if uploaded_photo:
   col2.success("Photo uploaded sucessfully!")
 
 
