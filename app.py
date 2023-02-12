cad0eb0953821f730d8c31fd85d622679e2c63be
MMIX2009 <btshilombo@comcast.net>
2023-02-11 21:28:06 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 col1, col2, col3 = st.columns([1,2,1])
 
-col1.markdown("# Welcome to my app!")
+col1.markdown("## Welcome to my app!")
 col1.markdown(" Here is some info about the app")
 
 uploaded_photo = col2.file_uploader("Upload an image")
